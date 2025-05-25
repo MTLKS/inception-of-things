23563e1948357a1701241fce50b0dc764d36aad3
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-05-25 13:51:06 +0800

    removed setting root password

--- bonus/scripts/install-gitlab.sh
+++ bonus/scripts/install-gitlab.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 
 # https://docs.gitlab.com/omnibus/installation/#set-up-the-initial-account
-sudo GITLAB_ROOT_PASSWORD="Baltig123!" EXTERNAL_URL="http://gitlab.example.com" apt install gitlab-ee
+sudo EXTERNAL_URL="http://gitlab.example.com" apt install gitlab-ee
 
 # https://docs.gitlab.com/omnibus/settings/nginx/#set-the-nginx-listen-port
 sudo sed -i "s/^[#[:space:]]*nginx\['listen_port'\] *= *.*/nginx['listen_port'] = 8081/" /etc/gitlab/gitlab.rb
