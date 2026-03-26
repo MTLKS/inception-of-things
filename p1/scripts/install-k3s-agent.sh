f2be5a973cd9da77dd4fbb7b4a410f2dd7fdfa26
MTLKS <43807318+MTLKS@users.noreply.github.com>
2026-03-26 21:33:26 +0800

    fix token

--- p1/scripts/install-k3s-agent.sh
+++ p1/scripts/install-k3s-agent.sh
@@ -4,4 +4,4 @@
 # https://docs.k3s.io/installation/configuration
 
 sudo ufw disable
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 12345 -i 192.168.56.111" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token $TOKEN -i 192.168.56.111" sh -s -
--- p1/scripts/install-k3s-server.sh
+++ p1/scripts/install-k3s-server.sh
@@ -5,7 +5,7 @@
 # https://docs.k3s.io/installation/configuration
 
 sudo ufw disable
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--token 12345 -i 192.168.56.110" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--token $TOKEN -i 192.168.56.110" sh -s -
 
 # -- INSTALL KUBECTL --
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
